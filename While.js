1cde9ab29d72f0d0279ff11d221947ccc95ebe9c
Nathalia Santos Ferreira <nathaliasantos.tds2.senai@gmail.com>
2024-03-13 19:41:24 -0300

    Comandos originais

---
+++ While.js
@@ -0,0 +1,14 @@
+// Contagem numérica simples 
+// Declaro uma variávelque inicialmente valerá 1
+let numero = 1;
+
+// Realizo o looping while/enquanto
+while (numero <= 10) {
+    // Exibo resultado
+    console.log(numero);
+    // Somo mais um ++
+    numero++;
+}
+
+// Exibo o resultado 
+console.log("Terminou");
