7d8b341bfb98b71f6e8d9e06a41852d6a5964209
Nathalia Santos Ferreira <nathaliasantos.tds2.senai@gmail.com>
2024-03-13 19:38:35 -0300

    Comandos originais

---
+++ Multiplicação.js
@@ -0,0 +1,9 @@
+// Declaro as variáveis 
+let a = 10;
+let b = 7;
+
+// Declaro uma variável com a operação
+let resultado = a * b;
+
+// Exibo o resultado 
+console.log("O resultado da multiplicação é: " + resultado);
