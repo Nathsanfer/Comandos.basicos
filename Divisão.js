54ee0a1800d0c2057d1f8d8e0706cfabc5b13f0d
Nathalia Santos Ferreira <nathaliasantos.tds2.senai@gmail.com>
2024-03-13 19:39:29 -0300

    Comandos originais

---
+++ Divisão.js
@@ -0,0 +1,9 @@
+// Declaro as variáveis 
+let a = 10;
+let b = 2;
+
+// Declaro uma variável com a operação
+let resultado = a / b;
+
+// Exibo o resultado 
+console.log("O resultado da divisão é: " + resultado);
