952112336a19086722c965742de9dfa3b48ac86d
Nathalia Santos Ferreira <nathaliasantos.tds2.senai@gmail.com>
2024-03-13 19:37:30 -0300

    Comandos originais

---
+++ Subtração.js
@@ -0,0 +1,9 @@
+// Declaro as variáveis 
+let a = 4;
+let b = 2;
+
+// Declaro uma variável com a operação
+let resultado = a - b;
+
+// Exibo o resultado 
+console.log("O resultado da subtração é: " + resultado);
